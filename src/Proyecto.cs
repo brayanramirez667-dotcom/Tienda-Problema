40acf485560bc703f69b2397e5e8bb23ce9f343b
luiscortes910 <luis.cortes910@pascualbravo.edu.co>
2026-03-03 08:37:31 -0500

    Update Proyecto.cs

--- src/Proyecto.cs
+++ src/Proyecto.cs
@@ -1,5 +1,3 @@
-# Tienda-Problema
-Una tienda en línea necesita un programa que, dados los datos de un pedido, determine su categoría de despacho y el costo de envío.
 using System;
 
 class Program
